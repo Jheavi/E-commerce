95363ca8bc26be1bba48c41f9e0b938221b4ed99
Jheavi <javierlaso213@hotmail.com>
2021-01-13 12:48:14 +0100

    remove unused method

--- backend/src/routes/itemListRouter.js
+++ backend/src/routes/itemListRouter.js
@@ -1,17 +1,14 @@
 const express = require('express');
-// const itemsController = require('../controllerks/itemsController');
 const itemsMongoController = require('../controllers/itemsMongoController');
 
 function itemsRouter(itemSchema) {
   const router = express.Router();
-  // const items = itemsController(ItemsStore);
   const itemsMongo = itemsMongoController(itemSchema);
 
   router.route('/')
     .get(itemsMongo.getMethod)
     .post(itemsMongo.postMethod)
-    .delete(itemsMongo.deleteMethod)
-    .patch(itemsMongo.patchMethod);
+    .delete(itemsMongo.deleteMethod);
 
   router.route('/:itemId')
     .get(itemsMongo.getByIdMethod);
